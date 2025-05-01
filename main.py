8213dbb6501dbb7125785535bb2ce70fa16270d1
Emre Dursun <126163359+emredursunn@users.noreply.github.com>
2025-05-01 10:03:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
-import sys
-if 'google' in sys.modules:
-    del sys.modules['google']
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from fastapi.responses import StreamingResponse
 from google import genai
-from google.generativeai.types import GenerateImageRequest, GenerateImagesConfig
-from io import BytesIO
+from google.genai import types
 from PIL import Image
+from io import BytesIO
 import os
 
+# Google API key ortam değişkeninden alınır
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
 app = FastAPI()
 
 @app.get("/generate")
-def generate_image(prompt: str = "Fuzzy bunnies in my kitchen"):
-    model = genai.GenerativeModel("imagen-3.0-generate-002")
-    response = model.generate_images(
+def generate(prompt: str = Query("Fuzzy bunnies in my kitchen")):
+    client = genai.Client()
+    response = client.models.generate_images(
+        model='imagen-3.0-generate-002',
         prompt=prompt,
-        config=GenerateImagesConfig(number_of_images=1),
+        config=types.GenerateImagesConfig(number_of_images=1),
     )
-    img_data = response.generated_images[0].image.image_bytes
-    return StreamingResponse(BytesIO(img_data), media_type="image/png")
+    
+    image_bytes = response.generated_images[0].image.image_bytes
+    return StreamingResponse(BytesIO(image_bytes), media_type="image/png")
